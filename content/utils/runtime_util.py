f5e20f9080c20206f12b4085d576d83b93f3a649
wjg10086 <wangjianguo@javasun.cn>
2026-05-16 09:13:02 +0800

    优化

--- content/utils/runtime_util.py
+++ content/utils/runtime_util.py
@@ -1,4 +1,7 @@
 import os
+import sys
+import uuid
+
 from base import configs as gc
 
 from langchain_core.runnables.config import var_child_runnable_config
@@ -89,3 +92,22 @@ def get_out_path(file_path):
     relative_path = file_path.replace(thread_dir, '')
 
     return relative_path
+
+# 生成uuid
+def get_uuid():
+    return uuid.uuid4().hex
+
+def get_platform():
+    platform = sys.platform
+    if platform.startswith("win"):
+        #print("这是 Windows 系统")
+        return 0
+    elif platform.startswith("linux"):
+        #print("这是 Linux 系统")
+        return 1
+    elif platform == "darwin":
+        #print("这是 macOS 系统")
+        return 2
+
+if __name__ == '__main__':
+    print(get_platform())
